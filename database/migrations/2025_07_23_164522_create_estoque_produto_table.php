338499a2bc92a47159244a7f9f366922d6e80021
vinicgobbi <vinicius.cgobbi2004@gmail.com>
2025-07-23 17:06:01 -0300

    feat: Adiciona migrations para todas as tabelas

---
+++ database/migrations/2025_07_23_164522_create_estoque_produto_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('estoque_produto', function (Blueprint $table) {
+            $table->id();
+            $table->string('nome_prod',50);
+            $table->string('desc_prod', 255)->nullable();
+            $table->string('cod_prod', 6)->unique();
+            $table->integer('quant_prod');
+            $table->integer('quant_min_prod');
+            $table->foreignId('almox_id')->constrained('estoque_almoxarifado');
+            $table->foreignId('categoria_id')->constrained('estoque_categoria');
+            $table->foreignId('grupo_id')->constrained('estoque_grupo');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('estoque_produto');
+    }
+};
