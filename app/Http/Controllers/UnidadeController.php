6c94d3b0497fffc0b24ac5ccb70a8c2a0979c89a
vinicgobbi <vinicius.cgobbi2004@gmail.com>
2025-08-06 16:36:31 -0300

    feat: Adiciona a capacidade de criar unidades

--- app/Http/Controllers/UnidadeController.php
+++ app/Http/Controllers/UnidadeController.php
@@ -12,4 +12,21 @@ class UnidadeController extends Controller
         $unidades = EstoqueUnidade::all();
         return view('unidade.get_unidade', compact('unidades'));
     }
+
+    public function createUnidade(Request $request)
+    {
+        $validated = $request->validate([
+            'nome' => 'required|string|max:3',
+            'descricao' => 'required|string|max:255'
+
+        ],[
+            'nome.required' => 'Digite uma Unidade antes de continuar',
+            'nome.max' => 'A unidade não pode ultrapassar 3 caracteres',
+            'descricao.required' => 'Digite a descrição da Unidade para prosseguir'
+        ]);
+
+        EstoqueUnidade::create($validated);
+
+        return redirect()->route('unidades')->with('success', 'Unidade criada com sucesso');
+    }
 }
