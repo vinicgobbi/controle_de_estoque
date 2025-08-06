861a8da414411ef8aa0f0badd99094ca393ceeff
vinicgobbi <vinicius.cgobbi2004@gmail.com>
2025-08-06 17:37:27 -0300

    fix: favicon com coloração incorreta

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -5,7 +5,7 @@
     
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="icon" type="image/png" href="/favicon_faesa.png">
+    <link rel="icon" type="image/png" href="{{ asset('faesa_favicon.png') }}">
     <title>Login</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
 
